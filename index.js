fcace920a0a9944044306c637d49e6b39c268913
Zach Ruiz <powersbooth10@gmail.com>
2022-08-30 17:15:46 -0500

    commit tree 7c340d4285ca02633e482abe5bc0ff2f1e683f48

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+   return drivers.slice(0, 2);
+
+}
+
+
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function(int) {
+    return function() {
+       return int * 5
+    } 
+}
+
+const fareDoubler = function createFareMultiplier(int) {
+    return int * 2
+//        return function() {
+//            return int * 2
+//        }
+    }
+
+const fareTripler = function createFareMultiplier(int) {
+    return int * 3
+//        return function() {
+//            return int * 2
+//        }
+    }
+        
+function selectDifferentDrivers(drivers, myFunction) {
+    if (myFunction === returnFirstTwoDrivers) {
+        return drivers = ['Antonia', 'Nuru']
+    }
+    else if (myFunction === returnLastTwoDrivers) {
+        return drivers = ['Amari', 'Mo']
+    }
+}
+
+
+returnLastTwoDrivers();
+createFareMultiplier();
+fareDoubler();
+fareTripler();
